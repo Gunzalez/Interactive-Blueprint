70e8d6e240b911da41435c4df349209f098428f7
Segun Konibire <segun.konibire@valtech.co.uk>
2016-06-14 15:52:33 +0100

    Initial commit

---
+++ website/js/presentation.js
@@ -0,0 +1,65 @@
+// JavaScript Document
+(function ($, window) {
+
+     //$("#main-wrap").blockScroll({
+     //     scrollDuration: [jQuery duration],
+     //     fadeBlocks: [bool],
+     //     fadeDuration: [jQuery duration]
+     //});
+
+    var blockScroller = $("#main-wrap").blockScroll({fadeBlocks:false});
+    $("#Intro-button").click(function() { blockScroller.goto(1); });
+    $("#About-button").click(function() { blockScroller.goto(3); });
+    $("#Features-button").click(function() { blockScroller.goto(4); });
+    $("#Download-button").click(function() { blockScroller.goto(7); });
+    $("#Install-button").click(function() { blockScroller.goto(8); });
+    $("#Customize-button").click(function() { blockScroller.goto(12); });
+    $("#Use-button").click(function() { blockScroller.goto(16); });
+    $("#Dot-button").click(function() { blockScroller.goto(18); });
+
+    //if ($('#frame3').visible(true)) {
+    //    console.log('Hey it\'s me');
+    //} else {
+    //    // The element is NOT visible, do something else
+    //}
+
+    var whichFrameIsVisible = function(){
+        $('.screen').each(function(index, obj){
+            if($('.stage',$(obj)).visible(true)){
+                //console.log($('.stage',$(obj)));
+                $(obj).addClass('active-frame');
+                console.log($(obj).attr('id'));
+            } else {
+                $(obj).removeClass('active-frame');
+            }
+        });
+    };
+
+    $('.trigger-1').on('click', function(evt){
+        evt.preventDefault();
+        $('.action-1').addClass('play-misty');
+    });
+
+    $.fn.scrollEnd = function(callback, timeout) {
+        $(this).scroll(function(){
+            var $this = $(this);
+            if ($this.data('scrollTimeout')) {
+                clearTimeout($this.data('scrollTimeout'));
+            }
+            $this.data('scrollTimeout', setTimeout(callback,timeout));
+        });
+    };
+
+    // how to call it (with a 1000ms timeout):
+    $(window).scrollEnd(function(){
+        //alert('stopped scrolling');
+        whichFrameIsVisible();
+    }, 100);
+
+    $(document).ready(function(){
+        $(this).scrollTop(0);
+    });
+
+  
+    
+}(jQuery, window));
